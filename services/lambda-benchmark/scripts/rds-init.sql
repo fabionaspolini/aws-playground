d0a287e042581a1c1b56ab9e44cc8857e00f23b7
Fábio Naspolini <fabionaspolini@gmail.com>
2023-04-15 20:10:38 -0300

    lambda benchmark data access rds init script

---
+++ services/lambda-benchmark/scripts/rds-init.sql
@@ -0,0 +1,14 @@
+create database lambda_test;
+
+drop table if exists pessoa;
+create table pessoa(
+    id uuid primary key,
+    nome varchar(100) not null,
+    data_nascimento date);
+
+insert into pessoa values(gen_random_uuid(), 'Fábio', '1990-01-01');
+insert into pessoa values(gen_random_uuid(), 'João da Silva', '1990-01-01');
+insert into pessoa values(gen_random_uuid(), 'Pedro Sampaio', '1990-01-01');
+insert into pessoa values(gen_random_uuid(), 'Mario Gomes', '1990-01-01');
+
+select * from pessoa;
