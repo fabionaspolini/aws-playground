85901989b2601eae2ef28dcde172d885cdc64584
Fábio Naspolini <fabionaspolini@gmail.com>
2023-04-15 21:18:08 -0300

    data access aot

---
+++ services/lambda-benchmark/src/data-access-aot/Function.cs
@@ -0,0 +1,131 @@
+using System.Text.Json.Serialization;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DataAccess.Aot;
+
+public class Function
+{
+    private static readonly IServiceProvider ServiceProvider;
+
+    static void ConfigureLogging(ILoggingBuilder builder) => builder
+        .AddSimpleConsole(x =>
+        {
+            x.SingleLine = true;
+            x.TimestampFormat = null;
+        });
+
+    private static async Task Main()
+    {
+        Func<SampleRequest, ILambdaContext, Task<SampleResponse[]>> handler = FunctionHandlerAsync;
+        await LambdaBootstrapBuilder.Create(handler, new SourceGeneratorLambdaJsonSerializer<LambdaFunctionJsonSerializerContext>())
+            .Build()
+            .RunAsync();
+    }
+
+    static Function()
+    {
+        var config = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+        var loggerFactory = LoggerFactory.Create(ConfigureLogging);
+        var logger = loggerFactory.CreateLogger<Function>();
+
+        var services = new ServiceCollection();
+        services.AddSingleton(config);
+        services.AddSingleton(loggerFactory);
+        services.AddLogging(ConfigureLogging);
+
+        var connectionString = config.GetValue<string>("ConnectionString");
+        logger.LogInformation($"ConnectionString: ${connectionString}");
+
+        services.AddDbContext<SampleContext>(opts => opts.UseNpgsql(connectionString));
+        services.AddScoped<SampleUseCase>();
+        ServiceProvider = services.BuildServiceProvider();
+    }
+
+    public static async Task<SampleResponse[]> FunctionHandlerAsync(SampleRequest request, ILambdaContext context)
+    {
+        context.Logger.LogInformation("Iniciando");
+        var result = new List<SampleResponse>();
+        for (var i = 1; i <= request.Count; i++)
+        {
+            using var scope = ServiceProvider.CreateScope();
+            var useCase = scope.ServiceProvider.GetRequiredService<SampleUseCase>();
+            await useCase.ExecuteAsync();
+        }
+        context.Logger.LogInformation("Concluído");
+        return result.ToArray();
+    }
+}
+
+[JsonSerializable(typeof(SampleRequest))]
+[JsonSerializable(typeof(SampleResponse[]))]
+public partial class LambdaFunctionJsonSerializerContext : JsonSerializerContext
+{
+}
+
+#pragma warning disable SYSLIB1037 // Source generator deserialization
+public record class SampleRequest(PersonRequest? Person, MathRequest? Math, int Count = 1, bool AddAllResponses = true);
+public record class PersonRequest(string FirstName, string LastName);
+public record class MathRequest(double A, double B);
+
+public record class SampleResponse(PersonResponse? Person, MathResponse? Math);
+public record class PersonResponse(string FullName, string WelcomeMessage);
+public record class MathResponse(double C);
+#pragma warning restore SYSLIB1037
+
+public class SampleUseCase
+{
+    private readonly ILogger<SampleUseCase> _logger;
+    private readonly SampleContext _context;
+
+    public SampleUseCase(ILogger<SampleUseCase> logger, SampleContext context)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task ExecuteAsync()
+    {
+        var pessoas = await _context.Pessoas.AsNoTracking().ToArrayAsync();
+        foreach (var pessoa in pessoas)
+            _logger.LogInformation($"{pessoa.Id}, {pessoa.Nome}, {pessoa.DataNascimento:dd/MM/yyyy}");
+    }
+}
+
+public class SampleContext : DbContext
+{
+    public SampleContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected SampleContext()
+    {
+    }
+
+    public DbSet<PessoaEntity> Pessoas { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var pessoaBuilder = modelBuilder.Entity<PessoaEntity>();
+        pessoaBuilder.ToTable("pessoa");
+        pessoaBuilder.HasKey(x => x.Id);
+        pessoaBuilder.Property(x => x.Id).HasColumnName("id");
+        pessoaBuilder.Property(x => x.Nome).HasColumnName("nome").HasMaxLength(100);
+        pessoaBuilder.Property(x => x.DataNascimento).HasColumnName("data_nascimento");
+    }
+}
+
+public class PessoaEntity
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = null!;
+    public DateTime DataNascimento { get; set; }
+}
