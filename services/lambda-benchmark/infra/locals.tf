63bd2fbe03a035f8e73331223e62d49dc6dd4815
Fábio Naspolini <fabionaspolini@gmail.com>
2023-04-17 12:32:25 -0300

    aot

--- services/lambda-benchmark/infra/locals.tf
+++ services/lambda-benchmark/infra/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  deploy_benchmark_basic_aot = true
-  deploy_benchmark_data_access_aot = true
+  deploy_benchmark_basic_aot = false
+  deploy_benchmark_data_access_aot = false
 
   rds_address       = aws_db_instance.default.address
   rds_port          = aws_db_instance.default.port
