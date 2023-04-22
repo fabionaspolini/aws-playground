9700866bb3e2d619f2df64e3c4fe270a86b7f38c
Fábio Naspolini <fabionaspolini@gmail.com>
2023-04-22 19:58:04 -0300

    nat gateway para testes com lambda

--- services/lambda-benchmark/infra/shared.tf
+++ services/lambda-benchmark/infra/shared.tf
@@ -19,6 +19,11 @@ data "aws_subnet" "zone_a" {
   availability_zone = "us-east-1a"
 }
 
+data "aws_subnet" "zone_b" {
+  vpc_id            = data.aws_vpc.default.id
+  availability_zone = "us-east-1b"
+}
+
 data "aws_security_group" "default" {
   vpc_id = data.aws_vpc.default.id
   name   = "default" # security group padrão da VPC possui o nome "default", mas exibe em branco no console aws.
@@ -39,3 +44,39 @@ data "aws_caller_identity" "current" {}
 #   bucket = aws_s3_bucket.temporary_deployment.id
 #   acl    = "private"
 # }
+
+
+resource "aws_eip" "lambda_samples" {
+  vpc = true
+  tags = {
+    Name = "lambda-samples"
+  }
+}
+
+resource "aws_nat_gateway" "lambda_samples" {
+  allocation_id = aws_eip.lambda_samples.id
+  subnet_id     = data.aws_subnet.zone_b.id
+
+  tags = {
+    Name = "lambda-samples"
+  }
+}
+
+
+resource "aws_route_table" "lambda_samples" {
+  vpc_id = data.aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.lambda_samples.id
+  }
+
+  tags = {
+    Name = "lambda-samples"
+  }
+}
+
+resource "aws_route_table_association" "lambda_samples" {
+  subnet_id      = data.aws_subnet.zone_a.id
+  route_table_id = aws_route_table.lambda_samples.id
+}
