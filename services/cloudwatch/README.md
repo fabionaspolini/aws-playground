7229277e6b3b478c4e1a9134d9ecd949a95e72c1
Fábio Naspolini <fabionaspolini@gmail.com>
2024-06-18 14:05:50 -0300

    cloudwatch: teste de métrica com console.writeline

--- services/cloudwatch/README.md
+++ services/cloudwatch/README.md
@@ -14,3 +14,46 @@ Biblioteca .NET para geração de métricas personalizadas.
 - Não possui dependência com AWSSDK
 - Newtonsoft.Json
 - Microsoft.Extensions.Logging.Abstractions
+
+
+
+## Exemplos
+
+JSON gerado pela métrica.
+
+```json
+{
+    "_aws": {
+        "Timestamp": 1718729497003,
+        "CloudWatchMetrics": [
+            {
+                "Namespace": "MyCustomMetric",
+                "Metrics": [
+                    {
+                        "Name": "Executions",
+                        "Unit": "Count"
+                    },
+                    {
+                        "Name": "InputsConvertidos",
+                        "Unit": "Count"
+                    },
+                    {
+                        "Name": "InputsNaoConvertidos",
+                        "Unit": "Count"
+                    }
+                ],
+                "Dimensions": [
+                    [
+                        "By Resource Name"
+                    ]
+                ]
+            }
+        ]
+    },
+    "executionEnvironment": "AWS_DOTNET_LAMDBA_TEST_TOOL_BLAZOR_0.15.1",
+    "By Resource Name": "EmbeddedMetricProducer::EmbeddedMetricProducer.Function::FunctionHandler",
+    "Executions": 1.0,
+    "InputsConvertidos": 1.0,
+    "InputsNaoConvertidos": 0.0
+}
+```
--- services/cloudwatch/src/embedded-metric-producer/Function.cs
+++ services/cloudwatch/src/embedded-metric-producer/Function.cs
@@ -1,6 +1,7 @@
 using Amazon.CloudWatch.EMF.Logger;
 using Amazon.CloudWatch.EMF.Model;
 using Amazon.Lambda.Core;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -26,7 +27,7 @@ public class Function
         var result = input.ToUpper();
 
         using var logger = new MetricsLogger();
-        logger.SetNamespace("MyCustomMetric");
+        logger.SetNamespace("MyCustomMetric.Library");
         var dimensionSet = new DimensionSet();
         dimensionSet.AddDimension("By Resource Name", context.FunctionName);
         logger.SetDimensions(dimensionSet);
@@ -36,7 +37,54 @@ public class Function
 
         // Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME")
 
+        WriteJsonAtConsole();
+
         return result;
+    }
+
+    private void WriteJsonAtConsole()
+    {
+        var time = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        // 1718729497003
+        var statistic = """
+                        {
+                            "_aws": {
+                                "Timestamp": 1718729497003,
+                                "CloudWatchMetrics": [
+                                    {
+                                        "Namespace": "MyCustomMetric.Manual",
+                                        "Metrics": [
+                                            {
+                                                "Name": "Executions",
+                                                "Unit": "Count"
+                                            },
+                                            {
+                                                "Name": "InputsConvertidos",
+                                                "Unit": "Count"
+                                            },
+                                            {
+                                                "Name": "InputsNaoConvertidos",
+                                                "Unit": "Count"
+                                            }
+                                        ],
+                                        "Dimensions": [
+                                            [
+                                                "By Resource Name"
+                                            ]
+                                        ]
+                                    }
+                                ]
+                            },
+                            "executionEnvironment": "AWS_DOTNET_LAMDBA_TEST_TOOL_BLAZOR_0.15.1",
+                            "By Resource Name": "EmbeddedMetricProducer::EmbeddedMetricProducer.Function::FunctionHandler",
+                            "Executions": 1.0,
+                            "InputsConvertidos": 1.0,
+                            "InputsNaoConvertidos": 0.0
+                    }
+                    """;
+        var json = JsonSerializer.Deserialize<JsonDocument>(statistic.Replace("1718729497003", time.ToString()));
+        var jsonMinified = JsonSerializer.Serialize(json, new JsonSerializerOptions { WriteIndented = false });
+        Console.WriteLine(jsonMinified);
     }
 }
 
