5237b6f71e157b70f0311c450e239297e725d949
Fábio Naspolini <fabionaspolini@gmail.com>
2024-11-03 23:29:30 -0300

    upgrade tf and aws module versions

--- services/kinesis/infra/backend.tf
+++ services/kinesis/infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.8.3"
+  required_version = ">= 1.9.8"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.49"
+      version = ">= 5.74.0"
     }
 
     archive = {
--- services/lambda/infra/backend.tf
+++ services/lambda/infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.9.8"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.72.0"
+      version = ">= 5.74.0"
     }
 
     archive = {
