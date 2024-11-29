ca95f932fa7835e45c945bc6a29ab5b86200fd09
Fábio Naspolini <fabionaspolini@gmail.com>
2024-11-29 12:40:33 -0300

    sample arch: cloudfront

---
+++ sample-arch/infra/teste-cf/backend.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.9.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.74.0"
+    }
+  }
+  backend "s3" {
+    # bucket = "terraform-state-$AWS_ACCOUNT_ID" # Não é permitido utilizar environment neste bloco. Estamos injetando no comando: terraform init -backend-config="bucket=terraform-state-${AWS_ACCOUNT_ID}"
+    key    = "aws-playground/sample-arch/teste-cf.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      managed-by = "terraform"
+      repo       = "aws-playground/sample-arch/teste-cf"
+    }
+  }
+}
---
+++ sample-arch/infra/teste-cf/cloudfront-response-headers-policies.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudfront_response_headers_policy" "image_png" {
+  name    = "content-type-png"
+  comment = "content-type: image/png"
+
+  security_headers_config {
+    content_type_options {
+      override = true
+    }
+  }
+  
+  custom_headers_config {
+    items {
+      header   = "Content-Type"
+      override = true
+      value    = "image/png"
+    }
+  }
+}
